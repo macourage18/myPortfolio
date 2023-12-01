14fffa94387512fafb5be138ecc6e1398d2a43a9
macourage18 <macourage17@gmail.com>
2023-12-01 21:11:12 +0100

    lastest

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,39 +6,36 @@ export const Footer = ()=> {
   
   return(
   <footer>
-    <div className="myfooter">
       <div className="arrange">
-      <div className="logof">
-          <Link to={"/"}> <img src={require("../media/macourage.png")} alt=""/> </Link>
-      </div>
-      <div>
-        <div className="mid2">
-          <Link to = "/"> Home </Link> 
-          <Link to = "/Portfolio"> Portfolio </Link>
-          <Link to = "/About"> About </Link>
-          <Link to = "/Contact"> Contact </Link>
+        <div className="logof">
+            <Link to={"/"}> <img src={require("../media/macourage.png")} alt=""/> </Link>
         </div>
-        <div className="icons3">
-          <Link to={"https://wa.link/lle71m"} > <Whatsapp className="icns3"/> </Link>
-          <Link to={"https://www.linkedin.com/in/courage-macjude-095a25220/"}> <Linkedin className="icns3"/> </Link>
-          <Link to={"https://github.com/macourage18"}> <Github className="icns3"/> </Link>
-          <Link to={"https://twitter.com/mac_courage"}> <Twitter className="icns3"/> </Link>
-    
-          <div>
+        <div>
+          <div className="mid2">
+            <Link to = "/"> Home </Link> 
+            <Link to = "/Portfolio"> Portfolio </Link>
+            <Link to = "/About"> About </Link>
+            <Link to = "/Contact"> Contact </Link>
+          </div>
+        </div>
+        <div className="con">
+          <div className="phn1">
             <Phone className="icns3" /> 
-            <h6 className="num">+234 8180729451, +234 9121175449</h6>
+            <h6 className="num1">+234 8180729451, +234 9121175449</h6>
           </div>
-          <div>
+          <div className="phn1">
             <EnvelopePaperHeart className="icns3" />
-            <h6 className="num">macourage17@gmail.com</h6>
+            <h6 className="num1">macourage17@gmail.com</h6>
           </div>
-          
-        </div>
+          <div className="icons3">
+            <Link to={"https://wa.link/lle71m"} > <Whatsapp className="icns3"/> </Link>
+            <Link to={"https://www.linkedin.com/in/courage-macjude-095a25220/"}> <Linkedin className="icns3"/> </Link>
+            <Link to={"https://github.com/macourage18"}> <Github className="icns3"/> </Link>
+            <Link to={"https://twitter.com/mac_courage"}> <Twitter className="icns3"/> </Link>
+          </div>
         </div>
       </div>
-    <p className="copy1">(c) 2023 Copyright @ macourage </p>
-    </div>
-</footer>
+  </footer>
 
   )
 }
--- src/components/home/Section2.jsx
+++ src/components/home/Section2.jsx
@@ -4,7 +4,9 @@ export const Section2 =() =>{
   return(
     <section className="sec2">
         <div className="image2">
+          <p className="txt3">====================</p>
           <p className="txt2"> My Works </p>
+          <p className="txt5">====================</p>
           <img src={require("../media/MacBook Pro 16_ - 5.png")} alt="" />
         </div>
         <div className="container">
