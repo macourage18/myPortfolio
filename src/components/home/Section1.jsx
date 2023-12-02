bdb6577959098071f72ad7bcd3fd6c7715871acb
macourage18 <macourage17@gmail.com>
2023-12-02 16:27:46 +0100

    latest

--- src/components/home/Section1.jsx
+++ src/components/home/Section1.jsx
@@ -5,31 +5,28 @@ export const Section1 =() =>{
     <section className="sec1">
       <div className=" intro">
         
-        <div className="txt1">
-          <div className="bg1"></div>
-          <h4>Hi There! I Am</h4>
-          <h1>Macourage</h1>
-          <p>Developer, UI/UX Designer <br/><span>Residing in Port Harcourt...</span></p>
-          
-        <div>
-          <Link to={'/Portfolio'}>
-            <button className="hireMe">
-              Hire Me !
-            </button>
-          </Link>
-          <Link to={'/Contact'}>
-            <button className="contactMe">
-              Contact Me !
-            </button>
-          </Link>
-        </div>
-      
-
-
-        </div>
-        <div className="image1">
-          <img src = {require ("../media/macourage2.png")} alt="" />
-        </div>
+          <div className="txt1">
+            <div className="bg1"></div>
+            <h4>Hi There! I Am</h4>
+            <h1>Macourage</h1>
+            <p>Developer, UI/UX Designer <br/><span>Residing in Port Harcourt...</span></p>
+            
+            <div>
+              <Link to={'/Portfolio'}>
+                <button className="hireMe">
+                  Hire Me !
+                </button>
+              </Link>
+              <Link to={'/Contact'}>
+                <button className="contactMe">
+                  Contact Me !
+                </button>
+              </Link>
+            </div>
+          </div>
+          <div className="image1">
+            <img src = {require ("../media/macourage2.png")} alt="" />
+          </div>
       </div>
     </section>
   )
