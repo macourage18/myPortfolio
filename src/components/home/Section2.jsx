afda68110d1685281e4e3ba3cfd901b6be7ced5e
macourage18 <macourage17@gmail.com>
2023-12-02 17:33:31 +0100

    latest

--- src/components/home/Section2.jsx
+++ src/components/home/Section2.jsx
@@ -1,4 +1,4 @@
-
+import { Link } from "react-router-dom"
 
 export const Section2 =() =>{
   return(
@@ -10,9 +10,41 @@ export const Section2 =() =>{
           <img src={require("../media/MacBook Pro 16_ - 5.png")} alt="" />
         </div>
         <div className="container">
-            <div className="card">
-
+         <div>
+          <Link to="">
+          <p className="see"> Visit Site </p>
+          </Link>
+          <div className="card">
+            <img src={require("../media/webimage.png")} alt="" />
+            <div className="des">
+                <p>Professional 4 Landing Page Portfolio</p>
             </div>
+          </div>
+         </div>
+         <div>
+          <Link to="">
+          <p className="see"> Visit Site </p>
+          </Link>
+          <div className="card">
+            <img src={require("../media/webimage.png")} alt="" />
+            <div className="des">
+            <p>Professional 4 Landing Page Portfolio</p>
+            </div>
+          </div>
+         </div>
+         <div>
+          <Link to="">
+          <p className="see"> Visit Site </p>
+          </Link>
+          <div className="card">
+            <img src={require("../media/webimage.png")} alt="" />
+            <div className="des">
+            <p>Professional 4 Landing Page Portfolio</p>
+            </div>
+          </div>
+         </div>
+           
+           
           </div>
       </section>
     
