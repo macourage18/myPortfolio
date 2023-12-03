2b5cc8d6d3ab2af275c716f045ab7e75826fc8c5
macourage18 <macourage17@gmail.com>
2023-12-03 13:35:40 +0100

    latest

--- src/components/portfolio/section2.jsx
+++ src/components/portfolio/section2.jsx
@@ -4,24 +4,33 @@ export const Section2 = ()=>{
       <section className="sec3">
         <div className="image4">
         <img src="" alt="" />
-          <p className="skill">Skills</p>
+          <p className="skill">== Skills ==</p>
         </div>
         <div className="skillset">
           <ul>
-            <h3>Backend</h3>
+            <h2>Backend</h2>
             <li>Javascript</li>
             <li>Python</li>
             <li>Django</li>
             <li>Django Rest Framework</li>
           </ul>
           <ul>
-            <h3>Frontend</h3>
+            <h2>Frontend</h2>
             <li>HTML</li>
             <li>CSS</li>
             <li>Tailwind CSS</li>
             <li>ReactJS</li>
             <li>NextJs</li>
+            <li>Javascript</li>
           </ul>
+          <ul>
+            <h2>Product Design</h2>
+            <li>Figma</li>
+            <li>Photoshop</li>
+            <li>Corel Draw</li>
+            <li>Inkscape</li>
+          </ul>
+        
         </div>
       </section>
     </div>
